627c6b3ddffcbe5b64dc6a9f9f810a1cda78f57b
kuligin <kuligin@yandex-team.ru>
2015-09-13 14:50:32 +0300

    KNN

--- ml/choose_your_own/your_algorithm.py
+++ ml/choose_your_own/your_algorithm.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python
 
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.neighbors import NearestCentroid
+from sklearn.metrics import accuracy_score
 
-features_train, labels_train, features_test, labels_test = makeTerrainData()
+if __name__ == "__main__":
+	features_train, labels_train, features_test, labels_test = makeTerrainData()
 
+	### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
+	### in together--separate them so we can give them different colors in the scatterplot,
+	### and visually identify them
+	grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+	bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+	grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+	bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
-### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
-### in together--separate them so we can give them different colors in the scatterplot,
-### and visually identify them
-grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-
-
-#### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
-#################################################################################
-
-
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
-
-
-
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+	#classifier
+	clf = NearestCentroid().fit(features_train, labels_train)
+	pred = clf.predict(features_test)
+	acc = accuracy_score(pred, labels_test)
+	print 'KNN accuracy: ', acc
+	
+	#### initial visualization
+	plt.xlim(0.0, 1.0)
+	plt.ylim(0.0, 1.0)
+	plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+	plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+	plt.legend()
+	plt.xlabel("bumpiness")
+	plt.ylabel("grade")
+	plt.savefig('fig.png')
+	
+	### your code here!  name your classifier object clf if you want the 
+	### visualization code (prettyPicture) to show you the decision boundary
+	try:
+		prettyPicture(clf, features_test, labels_test)
+	except NameError:
+		pass
