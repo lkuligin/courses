36c4d172213a706b934ad566e2b03319d5f82b85
kuligin <kuligin@yandex-team.ru>
2015-09-16 09:14:54 +0300

    un6

--- ml/regression/finance_regression.py
+++ ml/regression/finance_regression.py
@@ -30,21 +30,25 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+print 'Regression slope: ', reg.coef_
+print 'Regression intercept: ', reg.intercept_
+print 'R-score on training data: ', reg.score(feature_train, target_train)
+print 'R-score on testing data: ', reg.score(feature_test, target_test)
+print 'R-score on testing data: ', reg.score(feature_test, target_test)
 
+features_list2 = ["bonus", "long_term_incentive"]
+data2 = featureFormat( dictionary, features_list2, remove_any_zeroes=True)
+target2, features2 = targetFeatureSplit(data2)
+feature_train2, feature_test2, target_train2, target_test2 = train_test_split(features2, target2, test_size=0.5, random_state=42)
 
-### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
-### plots it correctly. Don't forget to change the test_color from "b" to "r"
-### to differentiate training points from test points.
-
-
-
-
-
-
-
+reg2 = linear_model.LinearRegression()
+reg2.fit(feature_train2, target_train2)
+print 'R-score on testing data on different factors: ', reg2.score(feature_test2, target_test2)
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -65,6 +69,9 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print 'Coefficient without outlier :', reg.coef_
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
